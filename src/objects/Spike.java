ccac60c9ee63762c9cb2b51628acf6903444740e
Bao Gia <acco5573@gmail.com>
2024-12-11 09:06:37 +0700

    UI change

---
+++ src/objects/Spike.java
@@ -0,0 +1,21 @@
+package objects;
+
+import main.Game;
+
+import java.awt.*;
+
+public class Spike extends GameObject {
+
+	public Spike(int x, int y, int objType) {
+		super(x, y, objType);
+		initHitbox(32, 16);
+		xDrawOffset = 0;
+		yDrawOffset = (int) (Game.SCALE * 16);
+		hitbox.y += yDrawOffset;
+
+	}
+
+	public void drawSpikeHitbox(Graphics g, int lvlOffset) {
+		super.drawHitbox(g,lvlOffset);
+	}
+}
